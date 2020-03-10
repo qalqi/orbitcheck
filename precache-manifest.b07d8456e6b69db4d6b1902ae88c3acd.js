f686612627059c7cb96569af9558d7363bf945d4
qalqi <qalqi@qalqi.com>
2020-03-10 21:52:33 +0530

    Updates

---
+++ precache-manifest.b07d8456e6b69db4d6b1902ae88c3acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e789b75c25f9ee40cd5de0ef2ef38642",
+    "url": "/orbitcheck/index.html"
+  },
+  {
+    "revision": "02e992fbc884f4872059",
+    "url": "/orbitcheck/static/css/main.b9a5901b.chunk.css"
+  },
+  {
+    "revision": "103018f8320387f6cae5",
+    "url": "/orbitcheck/static/js/2.e1df8447.chunk.js"
+  },
+  {
+    "revision": "4e54d00d77979900bb50bbbf21536dcc",
+    "url": "/orbitcheck/static/js/2.e1df8447.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e992fbc884f4872059",
+    "url": "/orbitcheck/static/js/main.62089b1f.chunk.js"
+  },
+  {
+    "revision": "61e633929466ab544f0e",
+    "url": "/orbitcheck/static/js/runtime-main.c3b46d91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.191dfef7c9d59d18b3c0230f4a172148.js"
+  "/orbitcheck/precache-manifest.b07d8456e6b69db4d6b1902ae88c3acd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/orbitcheck/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
