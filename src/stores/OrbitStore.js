b6bf4eff7165302d7c8992ed14736bd1a063b2bd
qalqi <qalqi@qalqi.com>
2020-03-10 12:49:38 +0530

    Added ipfs swarm options

--- src/stores/OrbitStore.js
+++ src/stores/OrbitStore.js
@@ -13,8 +13,7 @@ import PolkaAccessController from 'orbit-db-access-controllers/src/polka-access-
 const OrbitDB = require('orbit-db')
 let AccessControllers = require('orbit-db-access-controllers')
 
-
-const ipfsOptions = {
+const orbOptions = {
     "ipfs": {
         "preload": {
             "enabled": false
@@ -31,6 +30,42 @@ const ipfsOptions = {
         "orbit-dev-2020-10"
     ]
 }
+
+const ipfsOptions = {
+     EXPERIMENTAL: {
+      pubsub: true
+    },
+    relay: {
+      enabled: true,
+      hop: { enabled: false, active: false }
+    },
+    config: {
+      Addresses: {
+        Swarm: ['/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star/'],
+        API: '',
+        Gateway: ''
+      },
+      Discovery: {
+        MDNS: {
+          Enabled: false,
+          Interval: 10
+        },
+        webRTCStar: {
+          Enabled: true
+        }
+      },
+      Bootstrap: [
+        '/dns4/ams-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLer265NRgSp2LA3dPaeykiS1J6DifTC88f5uVQKNAd',
+        '/dns4/lon-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLMeWqB7YGVLJN3pNLQpmmEk35v6wYtsMGLzSr5QBU3',
+        '/dns4/sfo-3.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLPppuBtQSGwKDZT2M73ULpjvfd3aZ6ha4oFGL1KrGM',
+        '/dns4/sgp-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLSafTMBsPKadTEgaXctDQVcqN88CNLHXMkTNwMKPnu',
+        '/dns4/nyc-1.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLueR4xBeUbY9WZ9xGUUxunbKWcrNFTDAadQJmocnWm',
+        '/dns4/nyc-2.bootstrap.libp2p.io/tcp/443/wss/ipfs/QmSoLV4Bbm51jM9C4gDYZQ9Cy3U6aXMJDAbzgu2fzaDs64',
+        '/dns4/node0.preload.ipfs.io/tcp/443/wss/ipfs/QmZMxNdpMkewiVZLMRxaNxUeZpDUb34pWjZ1kZvsd16Zic',
+        '/dns4/node1.preload.ipfs.io/tcp/443/wss/ipfs/Qmbut9Ywz9YEDrz8ySBSgWyJk41Uvm2QJPhwDJzJyGFsD6'
+      ]
+    }
+  }
 
 class OrbitStore {
 
@@ -60,8 +95,8 @@ class OrbitStore {
         const verified = await Identities.verifyIdentity(identity);
         console.log(verified, 'verifyIdentity');
         this.identity = { ...identity.toJSON(), ...{ verified } };
-        this.ipfs = await IPFS.create();
-        let orbitOptions = { ...ipfsOptions }
+        this.ipfs = await IPFS.create(ipfsOptions);
+        let orbitOptions = { ...orbOptions }
 
         orbitOptions.directory = `directory`;
         orbitOptions.id = identity.toJSON().id;
